6e05d96e638d3972052a9cde7af57fbcdcb00eec
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-04-13 12:36:12 +0530

    Use systemd instead of upstart

--- start.sh
+++ start.sh
@@ -5,14 +5,21 @@ wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_e
 tar xvfz node_exporter-*.*-amd64.tar.gz
 cd node_exporter-*.*-amd64
 
-sudo ln -s node_exporter /usr/bin
+sudo cp node_exporter /usr/bin
 
-cat > /etc/init/node_exporter.conf <<EOF
-start on startup
-
-script
-   /usr/bin/node_exporter
-end script
+cat > /etc/systemd/system/node_exporter.service <<EOF
+[Unit]
+Description=Node Exporter
+After=network.target
+ 
+[Service]
+Type=simple
+ExecStart=/usr/bin/node_exporter
+ 
+[Install]
+WantedBy=multi-user.target
 EOF
 
-sudo service node_exporter start
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
