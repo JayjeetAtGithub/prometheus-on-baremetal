85efd01fca0e188e02a1c8a641e66eb1fb0b19eb
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-04-13 12:24:08 +0530

    Fix script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eu
 
-wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gztar xvfz node_exporter-*.*-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v1.1.2/node_exporter-1.1.2.linux-amd64.tar.gz
 tar xvfz node_exporter-*.*-amd64.tar.gz
 cd node_exporter-*.*-amd64
 
