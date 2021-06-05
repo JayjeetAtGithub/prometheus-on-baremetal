e380f252ae9e500e68af9a30f295d8540d3878c4
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-06-06 00:14:19 +0530

    Create 2 classes of nodes: client and storage

--- get_config.py
+++ get_config.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     with open('config.template.yml') as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
     
-    data['scrape_configs'][0]['static_configs'][0]['targets'] = hosts
+    data['scrape_configs'][0]['static_configs'][0]['targets'] = [hosts[0]]
+    data['scrape_configs'][1]['static_configs'][0]['targets'] = hosts[1:]
 
     with open('/tmp/config.yml', 'w') as f:
         yaml.dump(data, f, default_flow_style=False)
